edb2b70b3b1afef2d53eb9506566b2bd7ba065ae
FlLefe <lefebvre.florian9@gmail.com>
2024-07-10 14:56:35 +0200

    Ajout du dossier

---
+++ utils/supabase/verifyOtp.ts
@@ -0,0 +1,24 @@
+// utils/supabase/verifyOtp.ts
+"use server";
+
+import { createClient } from "@/utils/supabase/server";
+import { EmailOtpType } from "@supabase/supabase-js";
+
+export async function verifyOtp(token_hash: string, type: EmailOtpType) {
+  const supabase = createClient();
+  
+
+  const { error, data } = await supabase.auth.verifyOtp({
+    type,
+    token_hash,
+  });
+
+  if (error) {
+    return { error };
+  }
+
+  console.log("data", data);
+  console.log("error", error);
+
+  return { data } ;
+}
